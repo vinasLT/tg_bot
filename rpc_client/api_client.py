e268b77f2274b94f67e1d37873ec818e91dbe59b
peyro <peyrovskaaa@gmail.com>
2025-08-25 21:03:21 +0200

    Add phone number handling and update user model

---
+++ rpc_client/api_client.py
@@ -0,0 +1,29 @@
+import grpc
+
+from config import settings
+from rpc_client.base_client import BaseRpcClient, T
+from rpc_client.gen.python.auction.v1 import lot_pb2_grpc, lot_pb2
+
+
+class ApiRpcClient(BaseRpcClient[lot_pb2_grpc.LotServiceStub]):
+    def __init__(self):
+        super().__init__(server_url=settings.RPC_API_URL)
+
+    async def __aenter__(self):
+        await self.connect()
+        return self
+
+    def _create_stub(self, channel: grpc.aio.Channel) -> T:
+        return lot_pb2_grpc.LotServiceStub(channel)
+
+    async def get_lot_by_vin_or_lot_id(self, vin_or_lot_id: str, site: str = None) -> lot_pb2.GetLotByVinOrLotResponse:
+        data = lot_pb2.GetLotByVinOrLotRequest(vin_or_lot_id=vin_or_lot_id, site=site)
+        return await self._execute_request(self.stub.GetLotByVinOrLot, data)
+
+    async def get_current_bid(self, lot_id: int, site: str) -> lot_pb2.GetCurrentBidResponse:
+        data = lot_pb2.GetCurrentBidRequest(lot_id=lot_id, site=site)
+        return await self._execute_request(self.stub.GetCurrentBid, data)
+
+    async def get_sale_history(self, lot_id: int, site: str) -> lot_pb2.GetSaleHistoryResponse:
+        data = lot_pb2.GetSaleHistoryRequest(lot_id=lot_id, site=site)
+        return await self._execute_request(self.stub.GetSaleHistory, data)
